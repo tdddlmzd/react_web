c719d68f7fecbd270e630ae470e41d13c4cd3108
ningyuewen@360fob.com <12345678>
2020-09-01 12:18:06 +0800

    创建Home组件

---
+++ my_web/src/component/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Home extends React.Component {
+    render() {
+      return (
+        <div className='home_header'>
+            <ul className='home_route_head'>
+                <li>111111</li>
+            </ul>
+        </div>
+      )
+    }
+  }
+
+export default Home
