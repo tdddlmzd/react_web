b94013e08b16f4c4b78d1880e0d95cd17f15f79f
ningyuewen@360fob.com <12345678>
2020-09-01 17:12:28 +0800

    home完成

--- my_web/src/component/Home.js
+++ my_web/src/component/Home.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import NavBar from '../component/NavBar'
 class Home extends React.Component {
+    state = {
+      e:'233',
+      currentIndex: 1,
+    }
+    //滚动条滚动
+    anchor(index,togo){
+        this.setState({
+          currentIndex:index //改变当前选择状态
+        })
+        // 如果对应id的锚点存在，就跳转到锚点
+        let anchorElement = document.getElementById(togo)
+        if(anchorElement) {
+          anchorElement.scrollIntoView({block: 'start', behavior: 'smooth'})
+      }
+    }
+    //点击咨询
+    openQQ() {
+      window.open("https://wpa.qq.com/msgrd?v=3&uin=68381850&site=qq&menu=yes",'_blank')
+    }
     render() {
       return (
         <div className='home_main'>
@@ -9,28 +28,28 @@ class Home extends React.Component {
             <div className='home_head_text'>
                 <p className='home_text_p1'>鲸准API服务</p>
                 <p className='home_text_p2'>鲸准丰富的API服务，方便您快速构建自己的智能化航运！</p>
-                <p className='home_text_p3'>在线申请</p>
+                <p className='home_text_p3' onClick={() => this.openQQ()}>在线申请</p>
             </div>
           </div>
           <div className='home_content'>
             <ul className='home_content_ul'>
-              <li className='home_content_li'>
+              <li className='home_content_li' onClick={() => this.anchor(1,'a01')}>
                 <img src={require('../image/home/icon_chuanqi.png') }className='home_content_img' alt=''/>
                 <p className='home_content_text'>船期数据</p>
-                <p className='home_content_line'></p>
+                <p className={['home_content_line',(1 === this.state.currentIndex ? "home_content_blend_line" : '')].join(' ')}></p>
               </li>
-              <li className='home_content_li'>
+              <li className='home_content_li' onClick={() => this.anchor(2,'a02')}>
                 <img src={require('../image/home/icon_xianghuo.png') }className='home_content_img' alt=''/>
                 <p className='home_content_text'>箱货跟踪</p>
-                <p className='home_content_line'></p>
+                <p className={['home_content_line',(2 === this.state.currentIndex ? "home_content_blend_line" : '')].join(' ')}></p>
               </li>
-              <li className='home_content_li'>
+              <li className='home_content_li' onClick={() => this.anchor(3,'a03')}>
                 <img src={require('../image/home/icon_ditu.png') }className='home_content_img' alt=''/>
                 <p className='home_content_text'>地图式箱货跟踪</p>
-                <p className='home_content_line'></p>
+                <p className={['home_content_line',(3 === this.state.currentIndex ? "home_content_blend_line" : '')].join(' ')}></p>
               </li>
             </ul>
-            <div className="home_content_datamain">
+            <div className="home_content_datamain" id="a01">
                 <div className="home_content_flexmain">
                     <h2>船期数据</h2>
                     <ul className="home_content_datamm">
@@ -58,7 +77,7 @@ class Home extends React.Component {
                     </ul>
                 </div>
             </div>
-            <div className="home_content_datamain">
+            <div className="home_content_datamain" id="a02">
                 <div className="home_content_flexmain">
                     <h2>箱货跟踪</h2>
                     <ul className="home_content_datamm">
@@ -86,15 +105,21 @@ class Home extends React.Component {
                     </ul>
                 </div>
             </div>
-            <div className="home_content_datamain home_content_tsdatmain">
-            <div className="home_content_flexmain">
-                <h2>地图式箱货追踪</h2>
-                <div className="home_content_xhgz">
-                    <div className="home_content_dtsxh"></div>
-                    <p className="home_content_tidah">按提单号/箱号，查询运单的船期规划，船舶的当前位置，过去7天的航运轨迹，目的港ETA和剩余航程。</p>
-                </div>
+            <div className="home_content_datamain home_content_tsdatmain" id="a03">
+              <div className="home_content_flexmain">
+                  <h2>地图式箱货追踪</h2>
+                  <div className="home_content_xhgz">
+                      <div className="home_content_dtsxh"></div>
+                      <p className="home_content_tidah">按提单号/箱号，查询运单的船期规划，船舶的当前位置，过去7天的航运轨迹，目的港ETA和剩余航程。</p>
+                  </div>
+              </div>
             </div>
-        </div>
+            <div className="home_content_shucfoo">
+              <div className="home_content_shujh">船期 跟踪，就是鲸准 !</div>
+              <div className="home_content_buttons">
+                  <span onClick={() => this.openQQ()}>在线咨询</span>
+              </div>
+            </div>
           </div>
         </div>
       )
