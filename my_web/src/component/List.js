e72cbd98211a8b0457822ef7b0b1efc1ba469adf
tdddlmzd <tdddlmzd>
2020-09-06 10:14:03 +0800

    listshuj

--- my_web/src/component/List.js
+++ my_web/src/component/List.js
@@ -33,6 +33,10 @@ class List extends React.Component {
       isShowQis: false
     })
   }
+  qiClick(item){
+    console.log('11111111')
+    console.log(item)
+  }
   //目的港focues
   mudFoucus(){
     this.setState({
@@ -65,13 +69,15 @@ class List extends React.Component {
           directList.push({
             pathId: direct[i].pathId,
             id: direct[i].id,
-            staticEtdSort: direct[i].staticEtdSort,
-            staticEtaSort: direct[i].staticEtaSort,
+            scac: direct[i].scac,
+            staticEtdSort: this.getDateTime("mm-dd",direct[i].staticEtdSort),
+            staticEtaSort: this.getDateTime("mm-dd",direct[i].staticEtaSort),
+            staticEtdDate: direct[i].staticEtdDate ? direct[i].staticEtdDate : '-',
             polTerninal: direct[i].polTerninal,
             podTerninal: direct[i].podTerninal,
             transTime: direct[i].transTime,
-            vessel: direct[i].vessel,
-            voyage: direct[i].voyage,
+            vessel: direct[i].vessel ? direct[i].vessel : '-',
+            voyage: direct[i].voyage ? direct[i].voyage : '-',
             sameRoute: direct[i].sameRoute,
             routeCodeList: direct[i].routeCodeList,
             traceStatus: direct[i].traceStatus,
@@ -84,13 +90,15 @@ class List extends React.Component {
           transitList.push({
             pathId: transit[j].pathId,
             id: transit[j].id,
-            staticEtdSort: transit[j].staticEtdSort,
-            staticEtaSort: transit[j].staticEtaSort,
+            scac: transit[j].scac,
+            staticEtdSort: this.getDateTime("mm-dd",transit[j].staticEtdSort),
+            staticEtaSort: this.getDateTime("mm-dd",transit[j].staticEtaSort),
+            staticEtdDate: transit[j].staticEtdDate ? transit[j].staticEtdDate : '-',
             polTerninal: transit[j].polTerninal,
             podTerninal: transit[j].podTerninal,
             transTime: transit[j].transTime,
-            vessel: transit[j].vessel,
-            voyage: transit[j].voyage,
+            vessel: transit[j].vessel ? transit[j].vessel : '',
+            voyage: transit[j].voyage ? transit[j].voyage : '',
             sameRoute: transit[j].sameRoute,
             routeCodeList: transit[j].routeCodeList,
             traceStatus: transit[j].traceStatus,
@@ -98,13 +106,41 @@ class List extends React.Component {
           })          
         }
         this.setState({
-          direct:direct,
-          transit:transit
+          direct:directList,
+          transit:transitList
         })
       }
     }).catch(function (error) {
       console.log(error)
     })
+  }
+  getDateTime(fmt,val){
+    if(val){
+      let date = new Date(val)
+      let ret
+      let opt = {
+          "Y+": date.getFullYear().toString(),        // 年
+          "m+": (date.getMonth() + 1).toString(),     // 月
+          "d+": date.getDate().toString(),            // 日
+          "H+": date.getHours().toString(),           // 时
+          "M+": date.getMinutes().toString(),         // 分
+          "S+": date.getSeconds().toString()          // 秒
+          // 有其他格式化字符需求可以继续添加，必须转化成字符串
+      }
+      for (let k in opt) {
+          ret = new RegExp("(" + k + ")").exec(fmt);
+          if (ret) {
+              fmt = fmt.replace(ret[1], (ret[1].length === 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+          }
+      }
+      let a = fmt.split(' ')
+      let weekDay = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"]
+      let c = weekDay[new Date(Date.parse(a[0])).getDay()]
+      let weekDate = c + "(" + fmt + ")"
+      return weekDate
+    }else{
+      return '--'
+    }
   }
   componentDidMount(){
     this.getDate()
@@ -119,14 +155,15 @@ class List extends React.Component {
         <div className='list_content'>
           <div className='list_content_head'>
             <div className="list_content_divIpnut">
-              <input placeholder='请输入起始港' className='list_content_input' onFocus={()=>this.qisFoucus()} onBlur={()=>this.qisBlur()}/>
+              <input placeholder='请输入起始港' className='list_content_input' onFocus={()=>this.qisFoucus()} onBlur={()=>this.qisBlur()} readOnly/>
               {
                 isShowQis ? 
                 <div className='list_content_input_div1'>
                   {
                     qisCity.length > 0 ?
                       qisCity.map(item => {
-                        return <p className='list_content_input_div1_p' key={item.portCode} onMouseOver={this.mouseover.bind(this)} onMouseOut={this.mouseout.bind(this)}>{item.nameCn + '(' + item.nameEn + ')'}</p>
+                        var self = this
+                        return <p className='list_content_input_div1_p' key={item.portCode} onMouseOver={this.mouseover.bind(this)} onMouseOut={this.mouseout.bind(this)} onClick={this.qiClick.bind(this)}>{item.nameCn + '(' + item.nameEn + ')'}</p>
                       })
                       :
                       <p className='list_content_input_zwsj'>无数据</p>
@@ -172,36 +209,94 @@ class List extends React.Component {
                   <ul className='list_main_data_ul' key={item.id}>
                     <li className='list_main_direct_li direct__head1'>
                       {
-                        item.sameRoute.map(sameRoute =>{
+                        item.sameRoute.map((sameRoute,sameIndex) =>{
                           return (
-                            <p className='direct__head1_p' key={sameRoute.staticId}>
-                              <img src={require("../image/list/ACIC.png")} className='direct__head1_img' alt=''/>
-                              <span className='direct__head1_span'></span>
-                            </p>
+                            <div className='direct__head1_p' key={sameRoute.staticId}>
+                              <p style={{backgroundImage:`url(${require('../image/list/' + sameRoute.scac + '.png')})`}} className='direct__head1_img'></p>
+                              {
+                                sameIndex === item.sameRoute.length - 1
+                                ? 
+                                ""
+                                :
+                                <span className='direct__head1_span'></span>
+                              }
+                            </div>
                           )
                         })
                       }
                     </li>
                     <li className='list_main_direct_li direct__head2'>
-                      <p className='direct__head_p1'>周一(08-31)</p>
+                      <p className='direct__head_p1'>{item.staticEtdSort}</p>
                       <p className='direct__head_p2'>{item.polTerninal}</p>
                     </li>
                     <li className='list_main_direct_li direct__head3'>
-                    <p className='direct__head3_p1'>{item.transTime ? item.transTime + '天' : ''}</p>
+                      <p className='direct__head3_p1'>{item.transTime ? item.transTime + '天' : ''}</p>
                       <p className='direct__head3_p2'></p>
                       <p className='direct__head3_p3'>{item.traceStatus === 0 ? '直达' : '中转'}</p>
                     </li>
                     <li className='list_main_direct_li direct__head4'>
-                      <p className='direct__head_p1'>周一(08-31)</p>
+                      <p className='direct__head_p1'>{item.staticEtaSort}</p>
                       <p className='direct__head_p2'>{item.podTerninal}</p>
                     </li>
                     <li className='list_main_direct_li direct__head5'>
                       <div className='direct__head5_div'>
                         <div className='direct__head5_p1'>
                           <p className='direct__head5_p1_img'></p>
-                          <p className='direct__head5_p1_p'>ETD: 09-03 00:00</p>
+                          <p className='direct__head5_p1_p'>ETD: {item.staticEtdDate}</p>
                         </div>
-                        <div className='direct__head5_p2'>船名/航次:SINOTRANS DALIAN/2036E</div>
+                        <div className='direct__head5_p2'>船名/航次:{item.vessel + '/' + item.voyage}</div>
+                      </div>
+                    </li>
+                    <li className='list_main_direct_li direct__head6'>
+                      <p className='direct__head6_p'>详情</p>
+                    </li>
+                  </ul>
+                )
+              })
+            }
+            <div className='list_main_transit_p'>更多中转方案</div>
+            {
+              transit.map(item =>{
+                return (
+                  <ul className='list_main_data_ul' key={item.id}>
+                    <li className='list_main_direct_li direct__head1'>
+                      {
+                        item.routeCodeList.map((routeCode,routeIndex) =>{
+                          return (
+                            <div className='direct__head1_p' key={routeIndex}>
+                              <p style={{backgroundImage:`url(${require('../image/list/' + item.scac + '.png')})`}} className='direct__head1_img'></p>
+                              {
+                                routeIndex === item.routeCodeList.length - 1
+                                ? 
+                                ""
+                                :
+                                <span className='direct__head1_span'></span>
+                              }
+                            </div>
+                          )
+                        })
+                      }
+                    </li>
+                    <li className='list_main_direct_li direct__head2'>
+                      <p className='direct__head_p1'>{item.staticEtdSort}</p>
+                      <p className='direct__head_p2'>{item.polTerninal}</p>
+                    </li>
+                    <li className='list_main_direct_li direct__head3'>
+                      <p className='direct__head3_p1'>{item.transTime ? item.transTime + '天' : ''}</p>
+                      <p className='direct__head3_p2'></p>
+                      <p className='direct__head3_p3'>{item.traceStatus === 0 ? '直达' : '中转'}</p>
+                    </li>
+                    <li className='list_main_direct_li direct__head4'>
+                      <p className='direct__head_p1'>{item.staticEtaSort}</p>
+                      <p className='direct__head_p2'>{item.podTerninal}</p>
+                    </li>
+                    <li className='list_main_direct_li direct__head5'>
+                      <div className='direct__head5_div'>
+                        <div className='direct__head5_p1'>
+                          <p className='direct__head5_p1_img'></p>
+                          <p className='direct__head5_p1_p'>ETD: {item.staticEtdDate}</p>
+                        </div>
+                        <div className='direct__head5_p2'>船名/航次:{item.vessel + '/' + item.voyage}</div>
                       </div>
                     </li>
                     <li className='list_main_direct_li direct__head6'>
